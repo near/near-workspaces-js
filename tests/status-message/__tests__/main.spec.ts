4ac43a67b22ee31b2bbed66542f10059e926ce65
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2021-08-06 09:58:20 -0400

    docs: clarify view/patch state tests

--- tests/status-message/__tests__/main.spec.ts
+++ tests/status-message/__tests__/main.spec.ts
@@ -88,31 +88,50 @@ const schema = new Map([
   ],
 ]);
 
-test('Ali sets then gets status and patches state', async () => {
+test('View state', async () => {
   await sandboxRunner(async (sandbox: SandboxRuntime) => {
     const ali = sandbox.getAccount(ALI);
     const contract = sandbox.getContractAccount(CONTRACT);
     await ali.call(CONTRACT, "set_status", { message: "hello" })
-    
+
+    const state = await contract.viewState();
+
+    // Get raw value
+    let data = state.get_raw("STATE");
+
+    // deserialize from borsh
+    const statusMessage: StatusMessage = borsh.deserialize(schema, StatusMessage, data);
+
+    expect(statusMessage.records[0]).toStrictEqual(
+      new Record({ k: ali.accountId, v: 'hello' })
+    )
+  });
+});
+
+
+test('Patch state', async () => {
+  await sandboxRunner(async (sandbox: SandboxRuntime) => {
+    const ali = sandbox.getAccount(ALI);
+    const contract = sandbox.getContractAccount(CONTRACT);
+    // contract must have some state for viewState & patchState to work
+    await ali.call(CONTRACT, "set_status", { message: "hello" })
     // Get state
     const state = await contract.viewState();
     // Get raw value
-    let data = state.get_raw("STATE")//,{schema, type: StatusMessage});
-    // deserialize from borsh
-    const statusMessage: StatusMessage = borsh.deserialize(schema, StatusMessage, data);
+    let statusMessage = state.get("STATE", { schema, type: StatusMessage});
+
     // update contract state
     statusMessage.records.push(new Record({k: "alice.near", v: "hello world"}));
-    
+
     // serialize and patch state back to sandbox
     await contract.patchState("STATE", borsh.serialize(schema, statusMessage));
-    
+
     // Check again that the update worked
     const result = await contract.view("get_status", { account_id: "alice.near" })
     expect(result).toBe("hello world");
 
     // Can also get value by passing the schema
-    data = (await contract.viewState()).get("STATE", {schema, type: StatusMessage});
+    const data = (await contract.viewState()).get("STATE", {schema, type: StatusMessage});
     expect(data).toStrictEqual(statusMessage);
-    
   })
 });
