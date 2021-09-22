72f2f5d34d792e84eb18dd7183043125f4ce3c3c
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2021-09-22 15:47:24 -0400

    feat!(jest): add testMatch to default config (#59)

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,3 @@
 module.exports = require('./packages/jest/jest.config');
 
-module.exports.testMatch = ['**/__tests__/**/*.spec.ts'];
 module.exports.testPathIgnorePatterns.push('/bootstrap-starter/');
