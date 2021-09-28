2bcb0b1e18a6d72d61bba3d57d8015ae9a2c0d1d
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2021-09-28 12:55:32 -0400

    docs: update CLI help; rm ava.js

--- packages/ava/scripts/ava.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-const cli = require('ava/lib/cli.js'); // eslint-disable-line import/extensions
-// Currently just an alias will add config once upstream is fixed.
-cli.run();
--- packages/ava/scripts/cli.js
+++ packages/ava/scripts/cli.js
@@ -2,15 +2,15 @@
 
 const HELP = `Run near-runner-ava tests, or bootstrap a project. Examples:
 
-    near-runner-ava --bootstrap # Bootstrap a project with near-runner-ava.
-    near-runner-ava             # Run tests using 'ava', using custom config.
-    near-runner-ava --verbose   # All other flags get passed to the 'ava' CLI.
-    near-runner-ava -h, --help  # Print this. For Ava's help, use 'ava --help'.`;
+    near-runner-ava --bootstrap # Bootstrap a project with near-runner-ava
+    near-runner-ava             # Run tests using 'ava'
+    near-runner-ava --verbose   # All other flags get passed to the 'ava' CLI
+    near-runner-ava -h, --help  # Print this (for AVA's help, use 'ava --help')`;
 
 if (process.argv.includes('-h') || process.argv.includes('--help')) {
   console.log(HELP);
 } else if (process.argv.includes('--bootstrap')) {
   require('./bootstrap');
 } else {
-  require('ava/lib/cli.js').run(); // eslint-disable-line import/extensions;
+  require('ava/lib/cli.js').run(); // eslint-disable-line import/extensions
 }
