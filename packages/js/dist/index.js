b8e171cc548721128ffbfca61df6d259beabfe55
Willem Wyndham <willem@cs.umd.edu>
2021-09-30 09:38:29 -0400

    chore: update naj and add NEAR_PRINT_LOGS

--- packages/js/dist/index.js
+++ packages/js/dist/index.js
@@ -6,10 +6,26 @@ var __createBinding = (this && this.__createBinding) || (Object.create ? (functi
     if (k2 === undefined) k2 = k;
     o[k2] = m[k];
 }));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const process = __importStar(require("process"));
+if (!process.env.NEAR_PRINT_LOGS) {
+    process.env.NEAR_NO_LOGS = 'true';
+}
 __exportStar(require("./runner"), exports);
 __exportStar(require("./runtime"), exports);
 __exportStar(require("./utils"), exports);
