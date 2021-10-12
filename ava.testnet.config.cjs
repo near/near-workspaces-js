d91e7e54599e2308bc36f307dd53b6abce9c60c9
Willem Wyndham <wyndham@cs.unc.edu>
2021-10-12 14:06:10 -0400

    fix: ensure using near-runner-ava/ava.testnet.config.cjs

--- ava.testnet.config.cjs
+++ ava.testnet.config.cjs
@@ -1,4 +1,7 @@
-module.exports = require('./ava.config.cjs');
+module.exports = {
+  ...require('near-runner-ava/ava.testnet.config.cjs'),
+  ...require('./ava.config.cjs'),
+};
 
 module.exports.files.push(
   '!__tests__/02*',
--- packages/ava/ava.testnet.config.cjs
+++ packages/ava/ava.testnet.config.cjs
@@ -1,2 +1,6 @@
-require('process').env.NEAR_RUNNER_NETWORK = 'testnet';
+
 module.exports = require('./ava.config.cjs');
+
+module.exports.environmentVariables = {
+  NEAR_RUNNER_NETWORK: 'testnet',
+};
