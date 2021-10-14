e0819aea093b3ef8d4575add64e4dcbce03e7366
Willem Wyndham <wyndham@cs.unc.edu>
2021-10-14 10:33:20 -0400

    fix: check for account creation in patch state and try again if failed
    Throw if failed second time

--- packages/js/src/account/account.ts
+++ packages/js/src/account/account.ts
@@ -123,6 +123,13 @@ export class Account implements NearAccount {
 
     await account.sandbox_patch_state(records);
 
+    if (!await this.provider.accountExists(refContract)) {
+      await account.sandbox_patch_state(records);
+      if (!await this.provider.accountExists(refContract)) {
+        throw new Error(`Account ${refContract} does not exist after trying to patch into sandbox.`);
+      }
+    }
+
     if (withData) {
       const rawData = await rpc.viewStateRaw(account.accountId, '', blockQuery);
       const data = rawData.map(({key, value}) => ({
