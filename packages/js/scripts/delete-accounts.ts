c82bbb094082f0ac926ea0ba0dc8d78b0c31ac34
Serhii Volovyk <sergeyvolovyk@gmail.com>
2022-04-26 12:27:27 +0300

    fork deleted from delete-account script

--- packages/js/scripts/delete-accounts.ts
+++ packages/js/scripts/delete-accounts.ts
@@ -38,44 +38,43 @@ async function pMap<I, O=I>(array: I[], fn: (i: I) => Promise<O>): Promise<O[]> 
 
 async function main() {
   const worker = await Worker.init({network: 'testnet'});
+  const root = worker.rootAccount;
 
   const pattern = new RegExp(process.argv.length > 2 ? process.argv[2] : '');
 
-  worker.fork(async ({root}) => {
-    const accounts = (await fs.readdir(join(process.cwd(), '.near-credentials', 'workspaces', 'testnet')))
-      .map(s => parse(s).name);
-    const originalMap: Map<string, string[]> = new Map();
-    originalMap.set(root.accountId, []);
-    const accountMap: Map<string, string[]> = accounts.reduce((acc, accountId) => {
-      if (!pattern.test(accountId)) {
-        return acc;
-      }
-  
-      const parts = accountId.split('.');
-      const rootId = parts.pop()!;
-      if (!acc.has(rootId)) {
-        acc.set(rootId, []);
-      }
-  
-      if (parts.length > 0) {
-        acc.get(rootId)!.push(accountId);
-      }
-  
+  const accounts = (await fs.readdir(join(process.cwd(), '.near-credentials', 'workspaces', 'testnet')))
+    .map(s => parse(s).name);
+  const originalMap: Map<string, string[]> = new Map();
+  originalMap.set(root.accountId, []);
+  const accountMap: Map<string, string[]> = accounts.reduce((acc, accountId) => {
+    if (!pattern.test(accountId)) {
       return acc;
-    }, originalMap);
-  
-    await Promise.all([...accountMap.entries()].map(async ([rootAccountId, subaccounts]) => {
-      const rootAccount = root.getAccount(rootAccountId);
-      const key = await rootAccount.getKey() ?? undefined;
-      const txs = await pMap(subaccounts, async account => deleteAccount(account, rootAccount, key));
-      const failures = txs.filter(tx => tx?.failed).map(tx => tx?.summary());
-      if (failures.length > 0) {
-        console.log(failures);
-      }
-  
-      await deleteAccount(rootAccountId, rootAccount);
-    }));
-  });
+    }
+
+    const parts = accountId.split('.');
+    const rootId = parts.pop()!;
+    if (!acc.has(rootId)) {
+      acc.set(rootId, []);
+    }
+
+    if (parts.length > 0) {
+      acc.get(rootId)!.push(accountId);
+    }
+
+    return acc;
+  }, originalMap);
+
+  await Promise.all([...accountMap.entries()].map(async ([rootAccountId, subaccounts]) => {
+    const rootAccount = root.getAccount(rootAccountId);
+    const key = await rootAccount.getKey() ?? undefined;
+    const txs = await pMap(subaccounts, async account => deleteAccount(account, rootAccount, key));
+    const failures = txs.filter(tx => tx?.failed).map(tx => tx?.summary());
+    if (failures.length > 0) {
+      console.log(failures);
+    }
+
+    await deleteAccount(rootAccountId, rootAccount);
+  }));
 }
 
 main();
