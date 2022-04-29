42970daaf6148ce6edc6255b846cd82383d093b7
Bo Yao <bo@near.org>
2022-04-29 16:35:55 +0800

    lint

--- examples/simple-project/__tests__/test-status-message.ava.js
+++ examples/simple-project/__tests__/test-status-message.ava.js
@@ -7,36 +7,35 @@
  * on testnet by using the `test:sandbox` and `test:testnet` scripts in
  * package.json.
  */
- import {Worker, NEAR, NearAccount} from 'near-workspaces';
- import test from 'ava';
- 
- test.beforeEach(async t => {
-   // Init the worker and start a Sandbox server
-   const worker = await Worker.init();
- 
-   // Prepare sandbox for tests, create accounts, deploy contracts, etx.
-   const root = worker.rootAccount;
-   const contract = await root.createAndDeploy(
-     root.getSubAccount('status-message').accountId,
-     'contracts/status_message.wasm',
-     {initialBalance: NEAR.parse('3 N').toJSON()},
-   );
-   const ali = await root.createSubAccount('ali', {initialBalance: NEAR.parse('3 N').toJSON()});
- 
-   // Save state for test runs, it is unique for each test
-   t.context.worker = worker;
-   t.context.accounts = {root, contract, ali};
- });
- 
- test.afterEach(async t => {
-   // Stop Sandbox server
-   await t.context.worker.tearDown().catch(error => {
-     console.log('Failed to stop the Sandbox:', error);
-   });
- });
- 
+import {Worker, NEAR} from 'near-workspaces';
+import test from 'ava';
 
- test('Root gets null status', async t => {
+test.beforeEach(async t => {
+  // Init the worker and start a Sandbox server
+  const worker = await Worker.init();
+
+  // Prepare sandbox for tests, create accounts, deploy contracts, etx.
+  const root = worker.rootAccount;
+  const contract = await root.createAndDeploy(
+    root.getSubAccount('status-message').accountId,
+    'contracts/status_message.wasm',
+    {initialBalance: NEAR.parse('3 N').toJSON()},
+  );
+  const ali = await root.createSubAccount('ali', {initialBalance: NEAR.parse('3 N').toJSON()});
+
+  // Save state for test runs, it is unique for each test
+  t.context.worker = worker;
+  t.context.accounts = {root, contract, ali};
+});
+
+test.afterEach(async t => {
+  // Stop Sandbox server
+  await t.context.worker.tearDown().catch(error => {
+    console.log('Failed to stop the Sandbox:', error);
+  });
+});
+
+test('Root gets null status', async t => {
   const {root, contract} = t.context.accounts;
   const result = await contract.view('get_status', {account_id: root.accountId});
   t.is(result, null);
