6bf4f69580960c94168aa6309d44834b337b02bf
AlexKushnir1 <alex@alex-Precision-7510>
2025-02-19 17:51:52 +0200

    chore: stop tracking generated dist/files

--- packages/js/dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-export * from './worker';
-export * from './server';
-export * from './utils';
-export * from './types';
-export * from './account';
-export * from './transaction-result';
-export * from './jsonrpc';
-export * from 'near-units';
-//# sourceMappingURL=index.d.ts.map
